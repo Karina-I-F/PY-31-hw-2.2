cc774a2864c78d9f20118c46abedd7622eb27ec4
karina <ka_ri_n_ka@mail.ru>
2020-05-25 20:17:19 +0300

    Fixed indentation

--- Farm.py
+++ Farm.py
@@ -1,164 +1,175 @@
 class Animal:
-  """ Модель простого животного """
-  name = 'Животное'
-  weight = 0.1  # kg
-  gender = 'ж'
-  state = 'Голодный'
-  voice = 'Звуки неопределенного животного'
+    """ Модель простого животного """
+    name = 'Животное'
+    weight = 0.1  # kg
+    gender = 'ж'
+    state = 'Голодный'
+    voice = 'Звуки неопределенного животного'
 
-  def get_name(self):
-    """ Возвращает имя """
-    return self.name
+    def get_name(self):
+        """ Возвращает имя """
+        return self.name
 
-  def get_weight(self):
-    """ Возвращает вес """
-    return self.weight
+    def get_weight(self):
+        """ Возвращает вес """
+        return self.weight
 
-  def feed(self):
-    """ Кормит животное """
-    self.state = 'Голодный'
-    answer = input('Чтобы покормить животное введите любой символ: ')
-    if answer:
-      self.state = 'Сытый'
-      print('Животное накормлено.')
-    else:
-      print(self.state)
+    def feed(self):
+        """ Кормит животное """
+        self.state = 'Голодный'
+        answer = input('Чтобы покормить животное введите любой символ: ')
+        if answer:
+            self.state = 'Сытый'
+            print('Животное накормлено.')
+        else:
+            print(self.state)
 
-  def voice_recognition(self):
-    """ Определяет животное по голосу """
-    if self.voice == 'Га-га-га':
-      print('\nЭто гусь')
-    elif self.voice == 'Му-у':
-      print('\nЭто корова')
-    elif self.voice == 'Бе-е':
-      print('\nЭто овца')
-    elif self.voice == 'Ме-е':
-      print('\nЭто коза')
-    elif self.voice == 'Ко-ко-ко':
-      print('\nЭто курица')
-    elif self.voice == 'Кря-кря':
-      print('\nЭто утка')
-    else:
-      print('\nНе удалось определить животное по голосу')
+    def voice_recognition(self):
+        """ Определяет животное по голосу """
+        if self.voice == 'Га-га-га':
+            print('\nЭто гусь')
+        elif self.voice == 'Му-у':
+            print('\nЭто корова')
+        elif self.voice == 'Бе-е':
+            print('\nЭто овца')
+        elif self.voice == 'Ме-е':
+            print('\nЭто коза')
+        elif self.voice == 'Ко-ко-ко':
+            print('\nЭто курица')
+        elif self.voice == 'Кря-кря':
+            print('\nЭто утка')
+        else:
+            print('\nНе удалось определить животное по голосу')
 
 
 class Mammal(Animal):
-  """ Модель млекопитающего """
-  def __init__(self, gender: str):
-    self.gender = gender
-    self.milk = 'Молока нет'
+    """ Модель млекопитающего """
 
-  def milk(self):
-    """ Доит млекопитающее """
-    if self.gender == 'ж':
-      self.milk = 'Молоко есть'
-      answer = input('Чтобы подоить животное введите любой символ: ')
-      if answer:
+    def __init__(self, gender: str):
+        self.gender = gender
         self.milk = 'Молока нет'
-        print('Животное успешно подоено.')
-      else:
-          print(self.milk)
-    else:
-      print('Это животное не нужно доить.')
+
+    def milk(self):
+        """ Доит млекопитающее """
+        if self.gender == 'ж':
+            self.milk = 'Молоко есть'
+            answer = input('Чтобы подоить животное введите любой символ: ')
+            if answer:
+                self.milk = 'Молока нет'
+                print('Животное успешно подоено.')
+            else:
+                print(self.milk)
+        else:
+            print('Это животное не нужно доить.')
 
 
 class Sheep(Mammal):
-  """ Модель овцы """
-  def __init__(self, name: str, weight: float, gender: str):
-    self.name = name
-    self.weight = weight
-    self.gender = gender
-    self.voice = 'Бе-е'
+    """ Модель овцы """
 
-  def wool_picking(self):
-    """ Стрижёт овцу """
-    self.wool = 'Животное обросло'
-    answer = input('Чтобы постричь животное введите любой символ: ')
-    if answer:
-      self.wool = 'Шерсти нет'
-      print('Стрижка прошла успешно.')
-    else:
-      print(self.wool)
+    def __init__(self, name: str, weight: float, gender: str):
+        self.name = name
+        self.weight = weight
+        self.gender = gender
+        self.voice = 'Бе-е'
+
+    def wool_picking(self):
+        """ Стрижёт овцу """
+        self.wool = 'Животное обросло'
+        answer = input('Чтобы постричь животное введите любой символ: ')
+        if answer:
+            self.wool = 'Шерсти нет'
+            print('Стрижка прошла успешно.')
+        else:
+            print(self.wool)
 
 
 class Cow(Mammal):
-  """ Модель коровы """
-  def __init__(self, name: str, weight: float, gender: str):
-    self.name = name
-    self.weight = weight
-    self.gender = gender
-    self.voice = 'Му-у'
+    """ Модель коровы """
+
+    def __init__(self, name: str, weight: float, gender: str):
+        self.name = name
+        self.weight = weight
+        self.gender = gender
+        self.voice = 'Му-у'
 
 
 class Goat(Mammal):
-  """ Модель козы """
-  def __init__(self, name: str, weight: float, gender: str):
-    self.name = name
-    self.weight = weight
-    self.gender = gender
-    self.voice = 'Ме-е'
+    """ Модель козы """
+
+    def __init__(self, name: str, weight: float, gender: str):
+        self.name = name
+        self.weight = weight
+        self.gender = gender
+        self.voice = 'Ме-е'
 
 
 class Bird(Animal):
-  """ Модель птицы """
-  def __init__(self, gender):
-    self.gender = gender
-    self.eggs = 'Яиц нет'
+    """ Модель птицы """
 
-  def collecting_eggs(self):
-    """ Собирает яйца """
-    if self.gender == 'ж':
-      self.eggs = 'Яйца есть'
-      answer = input('Чтобы собрать яйца введите любой символ: ')
-      if answer:
+    def __init__(self, gender):
+        self.gender = gender
         self.eggs = 'Яиц нет'
-        print('Яйца успешно собраны.')
-      else:
-        print(self.eggs)
-    else:
-      print('Собирать нечего.')
+
+    def collecting_eggs(self):
+        """ Собирает яйца """
+        if self.gender == 'ж':
+            self.eggs = 'Яйца есть'
+            answer = input('Чтобы собрать яйца введите любой символ: ')
+            if answer:
+                self.eggs = 'Яиц нет'
+                print('Яйца успешно собраны.')
+            else:
+                print(self.eggs)
+        else:
+            print('Собирать нечего.')
 
 
 class Chicken(Bird):
-  """ Модель курицы """
-  def __init__(self, name: str, weight: float, gender: str):
-    self.name = name
-    self.weight = weight
-    self.gender = gender
-    self.voice = 'Ко-ко-ко'
+    """ Модель курицы """
+
+    def __init__(self, name: str, weight: float, gender: str):
+        self.name = name
+        self.weight = weight
+        self.gender = gender
+        self.voice = 'Ко-ко-ко'
 
 
 class Duck(Bird):
-  """ Модель утки """
-  def __init__(self, name: str, weight: float, gender: str):
-    self.name = name
-    self.weight = weight
-    self.gender = gender
-    self.voice = 'Кря-кря'
+    """ Модель утки """
+
+    def __init__(self, name: str, weight: float, gender: str):
+        self.name = name
+        self.weight = weight
+        self.gender = gender
+        self.voice = 'Кря-кря'
 
 
 class Goose(Bird):
-  """ Модель гуся """
-  def __init__(self, name: str, weight: float, gender: str):
-    self.name = name
-    self.weight = weight
-    self.gender = gender
-    self.voice = 'Га-га-га'
+    """ Модель гуся """
+
+    def __init__(self, name: str, weight: float, gender: str):
+        self.name = name
+        self.weight = weight
+        self.gender = gender
+        self.voice = 'Га-га-га'
 
 
 animal_dict = {}
 
+
 def get_total_weight(dict_for_sum):
-  """ Считает общий вес всех животных """
-  total_weight = sum(dict_for_sum.values())
-  print(f'\nОбщий вес всех животных - {total_weight} кг.')
+    """ Считает общий вес всех животных """
+    total_weight = sum(dict_for_sum.values())
+    print(f'\nОбщий вес всех животных - {total_weight} кг.')
+
 
 def get_heaviest_animal(dict_for_heaviest):
-  """ Выводит имя самого тяжёлого животного """
-  maximum = max(dict_for_heaviest.values())
-  for key, value in dict_for_heaviest.items():
-    if value == maximum:
-      print(f'Самое тяжёлое животное {key} - {maximum} кг.')
+    """ Выводит имя самого тяжёлого животного """
+    maximum = max(dict_for_heaviest.values())
+    for key, value in dict_for_heaviest.items():
+        if value == maximum:
+            print(f'Самое тяжёлое животное {key} - {maximum} кг.')
+
 
 goose1 = Goose('Серый', 5, 'м')
 goose1.voice_recognition()
